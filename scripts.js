c3706947540e93464844cbcc2ba6878cf96ebd7a
John Carmichael <jc@stackeducation.com>
2020-01-27 15:17:02 -0500

    The solution to Part Two of this exercise

--- scripts.js
+++ scripts.js
@@ -1,8 +1,26 @@
 function calculate() {
   var num1 = document.getElementById('num1').value
   var num2 = document.getElementById('num2').value
+  var opSelector = document.getElementById('operator')
 
-  var answer = parseInt(num1) + parseInt(num2)
+  var operator = opSelector.options[opSelector.selectedIndex].value
+
+  switch (operator) {
+    case 'add':
+      var answer = parseInt(num1) + parseInt(num2)
+      break;
+    case 'subtract':
+      var answer = parseInt(num1) - parseInt(num2)
+      break;
+    case 'multiply':
+      var answer = parseInt(num1) * parseInt(num2)
+      break;
+    case 'divide':
+      var answer = parseInt(num1) / parseInt(num2)
+      break;
+    default:
+      var answer = 'Invalid Operator'
+  }
 
   document.getElementById('answer').innerHTML = answer
 }
