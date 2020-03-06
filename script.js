c9b13a2085acc13e0ff84e22c320b932bde07e44
yahayaweekes <61118939+yahayaweekes@users.noreply.github.com>
2020-03-05 23:21:34 -0500

    revision#1

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
-function sum1() {
+function calculate() {
   var num1 = document.getElementById('num1').value
   var num2 = document.getElementById('num2').value
-  var outputElement = document.getElementById('output1')
+  var opSelector = document.getElementById('operator')
+  var operator = opSelector.options[opSelector.selectedIndex].value
 
-  outputElement.innerHTML = parseInt(num1) + parseInt(num2)
-}
+  switch (operator) {
+    case 'add':
+      var answer = parseInt(num1) + parseInt(num2)
+      break;
+    case 'subtract':
+      var answer = parseInt(num1) - parseInt(num2)
+      break;
+    case 'multiply':
+      var answer = parseInt(num1) * parseInt(num2)
+      break;
+    case 'divide':
+      var answer = parseInt(num1) / parseInt(num2)
+      break;
+    default:
+      var answer = 'Invalid Operator'
+  }
 
-function sum2() {
-  var num3 = document.getElementById('num3').value
-  var num4 = document.getElementById('num4').value
-  var outputElement = document.getElementById('output2')
-
-  outputElement.innerHTML = parseInt(num3) - parseInt(num4)
-}
-
-function sum3() {
-  var num5 = document.getElementById('num5').value
-  var num6 = document.getElementById('num6').value
-  var outputElement = document.getElementById('output3')
-
-  outputElement.innerHTML = parseInt(num5) * parseInt(num6)
-}
-
-function sum4() {
-  var num7 = document.getElementById('num7').value
-  var num8 = document.getElementById('num8').value
-  var outputElement = document.getElementById('output4')
-
-  outputElement.innerHTML = parseInt(num7) / parseInt(num8)
+  document.getElementById('answer').innerHTML = answer
 }
